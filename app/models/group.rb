602f317f9e745ea0daa6e13c1ef81b528905e3bc
Chengpeng Wang <chpengwang@gmail.com>
2018-01-27 23:21:55 +0800

    implement group user paticipated relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   validates :title, presence: true
 
   has_many :posts
-  
+  has_many :group_user_relationships
+  has_many :members, through: :group_user_relationships, source: :user
+
   belongs_to :user
 end
